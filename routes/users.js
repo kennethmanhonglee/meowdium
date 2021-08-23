425cbc4584476273dd07bcf09f438e49a829c8ce
Kristopher C. Castillo <kccastillo2@Kristophers-MacBook-Air-2.local>
2021-08-23 18:34:01 -0400

    Built up the users get route and started the user-signup pug

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcryptjs')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const { csrfProtection, asyncHandler } = require('./utils');
+
+const { User } = require('../db/models');
+
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+router.get("/signup", csrfProtection, asyncHandler (async (req, res, next) => {
+  const user = await User.build();
+
+  res.render('user-signup', {
+    title: "Sign Up",
+    user,
+    csrfToken: req.csrfToken()
+  });
+}));
+
+router.post("/signup", csrfProtection, asyncHandler (async (req, res, next) => {
+  res.send("respond with a resource");
+}));
 
 module.exports = router;
