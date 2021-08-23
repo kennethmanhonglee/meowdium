8750ed0fa3541629cd12f5863d9d144f61ca326b
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-08-23 13:05:17 -0700

    created User migration, model, and seeder files

---
+++ db/seeders/20210823195823-Users.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Users', [
+      //TODO - Make real seeds for users
+      // {username: '', email:'', hashedPassword: ''}
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
