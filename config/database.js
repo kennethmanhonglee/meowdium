b1bd0c5433fa38d5a84c01fb73964a2efc759714
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-08-25 09:04:19 -0700

    added code to deploy to heroku

--- config/database.js
+++ config/database.js
@@ -13,6 +13,12 @@ module.exports = {
   },
   production: {
     use_env_variable: 'postgres://jslfhogcrwxccz:546c3d795d5a078e4d16ac87a90815b3220d6a31bb66be91a857269294fd1987@ec2-52-45-179-101.compute-1.amazonaws.com:5432/da4giap3aj9nd7',
-    seederStorage: 'sequelize'
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
