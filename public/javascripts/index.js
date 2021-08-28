c42f2d6769667618e93409461c0d44cb1575196d
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-08-28 15:39:10 -0700

    separated index.js and pawsts.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,250 +1,61 @@
-document.addEventListener("DOMContentLoaded", async () => {
-  const userCommentForm = document.querySelector("#new-pawment-form");
-  const commentTextArea = document.querySelector('#content')
-  if (userCommentForm) {
+const catPuns = [
+    `Stay PAWsitive!`,
+    `You’ve got to be kitten me.`,
+    `Happy Purr–thday!`,
+    `I’m just kitten around.`,
+    `I think we should get meow-rried someday.`,
+    `That’s just claw-ful.`,
+    `You’re a fur-midable opponent.`,
+    `I’m so fur-tunate.`,
+    `Let me tell you a tail.`,
+    `That’s a paw-sibility.`,
+    `My cat is my best fur-end.`,
+    `That’s hiss-terical.`,
+    `He’ll go down in hiss-tory.`,
+    `You’ve got to be kitten me.`,
+    `Paw-don me.`,
+    `You look fur-miliar.`,
+    `That’s paw-some.`,
+    `Don’t fur-get to buy more catnip.`,
+    `I’ll love my cat fur-ever.`,
+    `That was a cat–astrophe.`,
+    `My cat is radi-claw.`,
+    `Dogs are in-furior to cats.`,
+    `I’m feline sad.`,
+    `You’ve got purr-sonality.`,
+    `Look at that meowntain.`,
+    `My cat is totally litter-ate.`,
+    `Whisker me away.`,
+    `I’m a glamourpuss.`,
+    `You’re so purr-suasive.`,
+    `My favorite color is purr-ple.`,
+    `Don’t be a sourpuss.`,
+    `Like my paw-jamas?`,
+    `Better call in claw-enforcement.`,
+    `I’ll have a meow-tini.`,
+    `I’m feline good.`,
+    `I need to take a paws.`,
+    `How claw-some is that?`,
+    `Can I paw-lease have that?`,
+    `I’ve got cattitude.`,
+    `Take meowt for lunch.`,
+    `I love my cat the meowst.`,
+    `I’ve done that be-fur.`,
+    `My cat is super cathletic.`,
+    `Never, efur do that again.`,
+    `Purr-haps we can cuddle later.`,
+    `My cat is so purr-ty.`,
+    `I love your purr-se.`,
+    `My cat wants a Furr-ari.`,
+    `I’ve got my thinking cat on.`,
+    `Wait a meow-ment.`
+];
 
-    userCommentForm.addEventListener("submit", async (e) => {
-      e.preventDefault();
-      const formData = new FormData(userCommentForm);
-      const content = formData.get('content');
-      const _csrf = formData.get('_csrf'); // TODO: test var naming
-      const body = { content, _csrf };
-      commentTextArea.value = '';
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+}
+document.addEventListener('DOMContentLoaded', (e) => {
+    const header = document.querySelector('h2');
+    console.log(header);
 
-      try {
-        const res = await fetch(`${window.location.href}/pawments`, { // TODO: change to API once functioning
-          method: "POST",
-          body: JSON.stringify(body),
-          headers: { "Content-Type": "application/json" },
-        });
-
-
-        if (!res.ok) {
-          throw 'Comment can\'t be empty.'
-        };
-
-        let {
-          id,
-          userId,
-          content,
-          userName,
-          updatedAt } = await res.json();
-
-        const pawmentsList = document.getElementById('pawments-list');
-
-        const pawmentDiv = document.createElement('div');
-        pawmentDiv.setAttribute("class", `pawment pawment-${id} blinder`);
-
-        const pawmenterInfoDiv = document.createElement('div');
-        pawmenterInfoDiv.setAttribute('class', 'pawmenter-info');
-
-        const commenterNameDiv = document.createElement('div');
-        commenterNameDiv.setAttribute('class', 'commenter-name');
-        const commenterNameLink = document.createElement('a')
-        commenterNameLink.setAttribute("href", `/users/${userId}`);
-        commenterNameLink.setAttribute('class', 'commenter-name-link')
-        commenterNameLink.textContent = userName;
-        commenterNameDiv.appendChild(commenterNameLink)
-        const pawmentDateDiv = document.createElement('div');
-        pawmentDateDiv.setAttribute('class', 'pawment-date');
-        pawmentDateDiv.textContent = updatedAt;
-        const hiddenIdInput = document.createElement('input');
-        hiddenIdInput.setAttribute('value', id);
-        hiddenIdInput.setAttribute('id', id);
-        hiddenIdInput.setAttribute('hidden', true);
-        pawmenterInfoDiv.append(commenterNameDiv, pawmentDateDiv, hiddenIdInput);
-        pawmentDiv.append(pawmenterInfoDiv);
-
-        const pawmentContentDiv = document.createElement('div');
-        pawmentContentDiv.setAttribute('class', 'pawment-content');
-        pawmentContentDiv.textContent = content;
-        pawmentDiv.append(pawmentContentDiv);
-
-        const pawmentButtonsDiv = document.createElement('div');
-        pawmentButtonsDiv.setAttribute('class', 'pawment-buttons');
-        const pawmentEditButton = document.createElement('button');
-        pawmentEditButton.setAttribute('value', id);
-        pawmentEditButton.textContent = 'Edit';
-        const pawmentDeleteButton = document.createElement('button');
-        pawmentDeleteButton.setAttribute('value', id);
-        pawmentDeleteButton.textContent = 'Delete';
-        pawmentEditButton.addEventListener('click', async (e) => {
-
-          pawmentContentDiv.setAttribute('contentEditable', 'true');
-          pawmentEditButton.setAttribute('hidden', 'true');
-          pawmentDeleteButton.setAttribute('hidden', 'true');
-
-          const editSubmitButton = document.createElement('button');
-          editSubmitButton.textContent = 'Submit'
-          const editCancelButton = document.createElement('button');
-          editCancelButton.textContent = 'Cancel'
-          pawmentButtonsDiv.append(editCancelButton, editSubmitButton);
-          editSubmitButton.addEventListener('click', async (e) => {
-            const apiPath = `/api/pawments/${id}/edit`;
-            const newContent = pawmentContentDiv.textContent
-            if (!newContent) return
-
-            const res = await fetch(apiPath, {
-              method: 'POST',
-              body: JSON.stringify({ content: newContent }),
-              headers: { 'content-type': 'application/json' }
-            })
-            // const { newContent } = await res.json();
-
-            pawmentContentDiv.textContent = newContent;
-            content = newContent;
-            pawmentContentDiv.setAttribute('contentEditable', 'false');
-            pawmentDeleteButton.removeAttribute('hidden');
-            pawmentEditButton.removeAttribute('hidden');
-            editSubmitButton.setAttribute('hidden', 'true');
-            editCancelButton.setAttribute('hidden', 'true');
-          })
-
-          editCancelButton.addEventListener('click', (e) => {
-            pawmentContentDiv.textContent = content;
-            pawmentContentDiv.setAttribute('contentEditable', 'false');
-            pawmentDeleteButton.removeAttribute('hidden');
-            pawmentEditButton.removeAttribute('hidden');
-            editSubmitButton.setAttribute('hidden', 'true');
-            editCancelButton.setAttribute('hidden', 'true');
-          })
-
-        })
-
-        pawmentDeleteButton.addEventListener('click', async (e) => {
-          const apiPath = `/api/pawments/${id}/delete`;
-
-          const res = await fetch(apiPath, {
-            method: 'POST',
-            body: JSON.stringify({
-              _key: '_value'
-            }),
-            headers: {
-              'content-type': 'application/json'
-            }
-          });
-
-          // dynamically remove comment
-          const pawmentDivToDelete = document.querySelector(`.pawment-${id}`);
-          pawmentDivToDelete.remove();
-        });
-        pawmentButtonsDiv.append(pawmentEditButton, pawmentDeleteButton);
-        pawmentDiv.append(pawmentButtonsDiv);
-
-        pawmentsList.prepend(pawmentDiv);
-        requestAnimationFrame(() => {
-          pawmentDiv.classList.remove("blinder")
-        })
-
-
-      } catch (err) {
-        //TODO - change way of handling error
-        console.log(err);
-      }
-    });
-  }
-
-  if (userCommentForm) {
-    const likeButton = document.querySelector('.likes-stats');
-    const likeDisplay = document.querySelector('.like-display');
-
-    likeButton.addEventListener('click', async (e) => {
-      const apiPath = `/api${window.location.pathname}/catnips`
-      const res = await fetch(apiPath, {
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          like: 'leelooo'
-        }),
-        method: 'POST'
-      })
-
-      const { deleted, catnipsCount } = await res.json();
-
-      if (deleted) {
-        // show unlike animation
-        likeDisplay.textContent = catnipsCount;
-        likeButton.classList.remove('clicked');
-      } else {
-        // show like animation
-        likeDisplay.textContent = catnipsCount;
-        likeButton.classList.add('clicked');
-      }
-    });
-  }
-
-
-  // deleting pawments
-  const pawmentsList = document.querySelector('#pawments-list');
-  for (let ele of pawmentsList.childNodes) {
-    // ele is a pawment div
-    if (ele.childNodes.length === 3) { //not an hr
-      // ele.childNodes - div.pawmenter-info, div.pawment-content, div.pawment-button
-      // lastchild - div.pawment-buttons
-      // value - hidden input that holds pawmentId rendered from get /pawsts/:id
-      const pawmentId = ele.childNodes[0].lastChild.id;
-      const pawmentDeleteButton = document.querySelector(`.delete-button-${pawmentId}`);
-      pawmentDeleteButton.addEventListener('click', async (e) => {
-        const apiPath = `/api/pawments/${pawmentId}/delete`;
-        const res = await fetch(apiPath, {
-          method: 'POST',
-          body: JSON.stringify({
-            _key: '_value'
-          }),
-          headers: {
-            'content-type': 'application/json'
-          }
-        });
-
-        // dynamically remove comment
-        const pawmentDivToDelete = document.querySelector(`.pawment-${pawmentId}`);
-        pawmentDivToDelete.remove();
-      })
-
-      const pawmentEditButton = document.querySelector(`.edit-button-${pawmentId}`)
-      pawmentEditButton.addEventListener('click', async (e) => {
-        const pawmentContentDiv = ele.childNodes[1];
-        let content = pawmentContentDiv.textContent
-        const editSubmitButton = document.querySelector(`.edit-submit-button-${pawmentId}`);
-        const editCancelButton = document.querySelector(`.edit-cancel-button-${pawmentId}`);
-        editSubmitButton.removeAttribute('hidden')
-        editCancelButton.removeAttribute('hidden')
-        pawmentEditButton.setAttribute('hidden', 'true')
-        pawmentDeleteButton.setAttribute('hidden', 'true')
-        pawmentContentDiv.setAttribute('contentEditable', 'true');
-        editSubmitButton.addEventListener('click', async (e) => {
-          const apiPath = `/api/pawments/${pawmentId}/edit`;
-          const newContent = pawmentContentDiv.textContent
-          if (!newContent) return
-
-          const res = await fetch(apiPath, {
-            method: 'POST',
-            body: JSON.stringify({ content: newContent }),
-            headers: { 'content-type': 'application/json' }
-          })
-
-          // const { newContent } = await res.json();
-
-          pawmentContentDiv.textContent = newContent;
-          content = newContent;
-          pawmentContentDiv.setAttribute('contentEditable', 'false');
-          pawmentDeleteButton.removeAttribute('hidden');
-          pawmentEditButton.removeAttribute('hidden');
-          editSubmitButton.setAttribute('hidden', 'true');
-          editCancelButton.setAttribute('hidden', 'true');
-        })
-
-        editCancelButton.addEventListener('click', (e) => {
-          pawmentContentDiv.textContent = content;
-          pawmentContentDiv.setAttribute('contentEditable', 'false');
-          pawmentDeleteButton.removeAttribute('hidden');
-          pawmentEditButton.removeAttribute('hidden');
-          editSubmitButton.setAttribute('hidden', 'true');
-          editCancelButton.setAttribute('hidden', 'true');
-        })
-
-      })
-    }
-  }
-
-  // TODO: EDITING COMMENTS RENDERED BY THE PUG FILE. TO BE EDITED.
-});
+})
