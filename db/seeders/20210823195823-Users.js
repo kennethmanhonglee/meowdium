63ac22435ccd73215092e3a50ec977804c994d4d
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-08-27 18:31:53 -0700

    made users and some posts

--- db/seeders/20210823195823-Users.js
+++ db/seeders/20210823195823-Users.js
@@ -13,11 +13,13 @@ module.exports = {
       }], {});
     */
     return queryInterface.bulkInsert('Users', [
-      //TODO - Make real seeds for users
       // REAL PW: aAaA1!1!
-      { userName: 'ken', email: 'ken@email.com', hashedPassword: '$2a$10$wUSUYOPrPlbfhKIypVoK6e6xUl1ZDgYukLsFHQCTbnRmK4zkQ8F8.', createdAt: new Date(), updatedAt: new Date() },
-      { userName: 'frank', email: 'frank@email.com', hashedPassword: '$2a$10$bx1kE4DTONAC4f4vOAEuW.vCbYePXTOlm4HNJpSSnhj0LN2Ovn5Nm', createdAt: new Date(), updatedAt: new Date() },
       { userName: 'tim_jetthauser', email: 'tim_jetthauser@email.com', hashedPassword: '$2a$10$Saz6YBC2t2cZQM.8bEyr3uZiAzetIpnMjYT4ST0DAwSXwzOGryF/a', createdAt: new Date(), updatedAt: new Date() },
+      { userName: 'harmless_potato', email: 'potato@email.com', hashedPassword: '$2a$10$wUSUYOPrPlbfhKIypVoK6e6xUl1ZDgYukLsFHQCTbnRmK4zkQ8F8.', createdAt: new Date(), updatedAt: new Date() },
+      { userName: 'milkshake', email: 'boyz@my.yard', hashedPassword: '$2a$10$bx1kE4DTONAC4f4vOAEuW.vCbYePXTOlm4HNJpSSnhj0LN2Ovn5Nm', createdAt: new Date(), updatedAt: new Date() },
+      { userName: 'jenny_panini', email: 'jenny_panini@email.com', hashedPassword: '$2a$10$Saz6YBC2t2cZQM.8bEyr3uZiAzetIpnMjYT4ST0DAwSXwzOGryF/a', createdAt: new Date(), updatedAt: new Date() },
+      { userName: 'notCrazyCatLady', email: 'lady@email.com', hashedPassword: '$2a$10$Saz6YBC2t2cZQM.8bEyr3uZiAzetIpnMjYT4ST0DAwSXwzOGryF/a', createdAt: new Date(), updatedAt: new Date() },
+      { userName: 'blob_marley', email: 'ipaintgood@email.com', hashedPassword: '$2a$10$Saz6YBC2t2cZQM.8bEyr3uZiAzetIpnMjYT4ST0DAwSXwzOGryF/a', createdAt: new Date(), updatedAt: new Date() },
     ]);
   },
 
