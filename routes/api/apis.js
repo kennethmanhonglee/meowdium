650fedb3018311e6a3e774c040770540b9e8e9b4
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-08-28 09:18:48 -0700

    added slight changes

--- routes/api/apis.js
+++ routes/api/apis.js
@@ -7,12 +7,12 @@ const { csrfProtection, asyncHandler } = require('../utils');
 
 const pawmentValidators = [
     check('content')
-      .exists({ checkFalsy: true })
-      .withMessage('Pawment can\'t be empty.')
-  ];
+        .exists({ checkFalsy: true })
+        .withMessage('Pawment can\'t be empty.')
+];
 
 router.post('/pawsts/:id(\\d+)/catnips', asyncHandler(async (req, res) => {
-    // TODO - verify that the user is logged in
+    // verify that the user is logged in
     if (!res.locals.authenticated) {
         return res.status(404).json('You are not logged in!');
     }
@@ -25,7 +25,11 @@ router.post('/pawsts/:id(\\d+)/catnips', asyncHandler(async (req, res) => {
             userId
         }
     })
-    let catnipsCount = await Catnip.count();
+    let catnipsCount = await Catnip.count({
+        where: pawstId
+    });
+
+    console.log(catnipsCount);
 
     if (existingCatnip) {
         // unlike post - delete catnip from db, send back 'deleted'
@@ -68,30 +72,30 @@ router.post('/pawments/:id(\\d+)/delete', asyncHandler(async (req, res) => {
 
 router.post('/pawments/:id(\\d+)/edit', pawmentValidators, asyncHandler(async (req, res) => {
     if (!res.locals.authenticated) {
-      return res.status(401).json('You need to be logged in to use this feature!');
+        return res.status(401).json('You need to be logged in to use this feature!');
     }
     const { content } = req.body
     const pawmentId = parseInt(req.params.id, 10);
     const pawment = await Pawment.findByPk(pawmentId);
     // console.log('CONTENT!!!!!!', content);
     if (res.locals.user.id !== pawment.userId) {
-      return res.status(404).json('Unauthorized User.')
+        return res.status(404).json('Unauthorized User.')
     }
     const validationErrors = validationResult(req);
     if (validationErrors.isEmpty()) {
-      await pawment.update({
-        content,
-        updatedAt: pawment.updatedAt
-      })
-      return res.json({
-        pawment,
-        newContent: content,
-        updatedAt: pawment.updatedAt
-      })
+        await pawment.update({
+            content,
+            updatedAt: pawment.updatedAt
+        })
+        return res.json({
+            pawment,
+            newContent: content,
+            updatedAt: pawment.updatedAt
+        })
     } else {
         return res.status(406).json({
             emptyComment: true
-          });
+        });
     }
 }));
 
