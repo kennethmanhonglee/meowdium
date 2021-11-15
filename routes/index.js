1e40d1f8f0aecce523052dc4708bcec85808dcff
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-11-15 09:53:42 -0800

    created splash html, css, and route to show the splash page

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 const { csrfProtection, asyncHandler } = require("./utils");
 
 const { User, Pawst } = require("../db/models");
 
-
 /* GET home page. */
-router.get('/', asyncHandler(async (req, res, next) => {
-  const posts = await Pawst.findAll({
-    order: [['createdAt', 'DESC']],
-    include: [User],
-    limit: 15
-  });
+router.get(
+  "/",
+  asyncHandler(async (req, res, next) => {
+    if (!res.locals.authenticated) res.render("splash");
 
-  return res.render("index", {
-    title: 'Meowdium',
-    posts
-  });
-}));
+    const posts = await Pawst.findAll({
+      order: [["createdAt", "DESC"]],
+      include: [User],
+      limit: 15,
+    });
+
+    return res.render("index", {
+      title: "Meowdium",
+      posts,
+    });
+  })
+);
 
 module.exports = router;
